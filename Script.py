1a36a90b83d0ed0b8d148184f8d78d0ece225d08
Javier Álvarez <javieralvarez9898@gmail.com>
2021-11-21 17:29:32 +0100

    Creation of the Readme file and describe tables.

--- Script.py
+++ Script.py
@@ -2,12 +2,13 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
 #%%
 data = pd.read_csv('Data/lending_club_loan_dataset.csv')
 data.head()
 
 print('Total number of rows -> ' + str(data.shape[0]))
 print('Total number of variables -> ' + str(data.shape[1]))
+# %%
+data.describe().T
 
-#%%
+# %%
